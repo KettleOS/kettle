1e650c5592160f4ee9d1baaedf845467e62b13ba
Sylv <contact@sylv.gay>
2024-10-27 16:55:16 -0400

    fix: clarify and consolidate SAFETY comment in `_init_rt`

--- kettle-kernel/src/_arch/aarch64/boot.rs
+++ kettle-kernel/src/_arch/aarch64/boot.rs
@@ -8,12 +8,10 @@ global_asm!(include_str!("boot.s"));
 #[no_mangle]
 #[doc = include_str!("../_init_rt.md")]
 pub unsafe extern "C" fn _init_rt() -> ! {
+	// SAFETY:
+	// These functions are only called once inside _init_rt.
 	unsafe {
-		// SAFETY:
-		// This function is only called once.
 		kernel_init();
-		// SAFETY:
-		// This function is only called once inside _init_rt.
 		kernel_main();
 	}
 
