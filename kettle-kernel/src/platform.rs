727c1fa405c59e481f9ba02101a6968c391ca998
Sylv <contact@sylv.gay>
2024-10-26 21:38:47 -0400

    refactor: specify per-arch platform-specific exports

--- kettle-kernel/src/platform.rs
+++ kettle-kernel/src/platform.rs
@@ -4,7 +4,17 @@ mod _virt;
 #[cfg(feature = "platform_virt")]
 use _virt as internal_platform;
 
-#[allow(unused_imports, unused_braces)]
-pub use internal_platform::{BOOT_CORE_ID};
-
+#[cfg(feature = "platform_virt")]
 pub mod virt {}
+
+/// Per-architecutre platform-specific exports.
+#[cfg(target_arch = "aarch64")]
+#[allow(unused_imports, unused_braces)]
+pub mod aarch64 {
+	pub use super::internal_platform::{BOOT_CORE_ID};
+}
+
+/// Per-architecutre platform-specific exports.
+#[cfg(target_arch = "x86_64")]
+#[allow(unused_imports, unused_braces)]
+pub mod x86_64 {}
