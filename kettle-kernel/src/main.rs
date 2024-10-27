97ef04d3f854235767657574d03dc73f045c4414
Sylv <contact@sylv.gay>
2024-10-26 20:50:50 -0400

    fix(BREAKING): remove ABI from `kernel_main`

--- kettle-kernel/src/main.rs
+++ kettle-kernel/src/main.rs
@@ -6,8 +6,8 @@ mod cpu;
 mod panic;
 mod platform;
 
-/// The main kernel entrypoint. This is callable from FFI and is non-mangled.
+/// The main kernel entrypoint. This function is non-mangled for debug purposes; however, it is incompatible with FFI.
 ///
-/// However, it is ill-advised to call this function directly. Instead, implement an [_init_rt](cpu::boot::_init_rt) wrapper and call [kernel_main] from there.
+/// It is ill-advised to call this function directly. Instead, implement an [_init_rt](cpu::boot::_init_rt) wrapper and call [kernel_main] from there.
 #[no_mangle]
-pub unsafe extern "C" fn kernel_main() {}
+pub unsafe fn kernel_main() {}
