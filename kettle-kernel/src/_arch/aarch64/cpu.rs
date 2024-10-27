776c636f7c334eabac0d2ff9b718a1df9be61aca
Sylv <contact@sylv.gay>
2024-10-26 22:55:05 -0400

    refactor: separate and expose assembly instruction functions

--- kettle-kernel/src/_arch/aarch64/cpu.rs
+++ kettle-kernel/src/_arch/aarch64/cpu.rs
@@ -1,8 +1,17 @@
 use core::arch::asm;
 
+/// # Safety
+/// You must ensure that your usecase is thread-safe.
+#[inline(always)]
+pub unsafe fn wfe() {
+	unsafe { asm!("wfe") };
+}
+
 #[inline(always)]
 pub fn wait_forever() -> ! {
 	loop {
-		unsafe { asm!("wfe") };
+		// SAFETY:
+		// After waiting for events, the function loops, waiting indefinitely.
+		unsafe { wfe() };
 	}
 }
--- kettle-kernel/src/cpu.rs
+++ kettle-kernel/src/cpu.rs
@@ -6,3 +6,9 @@ mod arch_cpu;
 
 #[allow(unused_imports, unused_braces)]
 pub use arch_cpu::{wait_forever};
+
+#[cfg(target_arch = "aarch64")]
+#[allow(unused_imports, unused_braces)]
+pub mod aarch64 {
+	pub use super::arch_cpu::{wfe};
+}
