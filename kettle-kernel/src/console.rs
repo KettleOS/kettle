5dc8e002238f5e693b70c7a4d9965ef9fb2ecf3c
Sylv <contact@sylv.gay>
2024-10-27 02:31:01 -0400

    refactor: re-export `QEMUConsole` for `aarch64`

--- kettle-kernel/src/console.rs
+++ kettle-kernel/src/console.rs
@@ -2,6 +2,12 @@
 #[path = "_arch/aarch64/console.rs"]
 mod arch_console;
 
+#[cfg(target_arch = "aarch64")]
+#[allow(unused_imports, unused_braces)]
+pub mod aarch64 {
+	pub use super::arch_console::{QEMUConsole};
+}
+
 pub trait Console: core::fmt::Write {}
 
 /// Returns the global console that printing macros use.
